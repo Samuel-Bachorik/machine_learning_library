49eaa433c1ed603ddc618736613de5d9c79a6de1
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2023-01-20 16:06:29 +0100

    Update save_load_model.py

--- save_load_model.py
+++ save_load_model.py
@@ -3,41 +3,32 @@ import numpy as np
 class save_load:
 
     def load_weights(self,model,filename):
-
         data = np.load(str(filename))
         lst = data.files
 
         counter = 0
+        for i in range(len(model.layers)):
+            if model.layers[i].__class__.__name__ == "Linearlayer":
+                model.layers[i].weight = data[lst[counter]]
+                counter+=1
 
-        for i in range(len(lst)):
-
-            if model.layers[counter].__class__.__name__ == "Linearlayer":
-                model.layers[counter].weight = data[lst[i]]
-
-                if model.layers[counter].b:
-                    model.layers[counter].bias = data[lst[i+1]]
-
-                    i+=1
-
-            if counter == len(model.layers)-1:
-                break
-
-            counter += 1
-
+                if model.layers[i].b:
+                    model.layers[i].bias = data[lst[counter]]
+                    counter+=1
 
 
     def save_weights(self,model,filename):
 
         model_weights = {}
-
+        counter = 0
         for i in range(len(model.layers)):
             if model.layers[i].__class__.__name__ == "Linearlayer":
-                model_weights[str(i)] = model.layers[i].weight
+                model_weights[str(counter)] = model.layers[i].weight
+                counter+=1
 
                 if model.layers[i].b:
-                    model_weights[str(i+1)] = model.layers[i].bias
+                    model_weights[str(counter)] = model.layers[i].bias
+                    counter+=1
 
-                    i+=1
 
         np.savez_compressed(str(filename), **model_weights)
-
