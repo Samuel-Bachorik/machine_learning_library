5f57db82293d5849009698da6935f46bf7e356ad
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2023-01-22 21:52:38 +0100

    Update run_training.py

--- run_training.py
+++ run_training.py
@@ -12,37 +12,39 @@ for z in range(10):
 
 
 if __name__ == '__main__':
+    
+    batch_size  = 512
+    loader      = ImagesLoader(batch_size)
+    dataset     = loader.get_dataset(training_paths, training=False)
 
-    batch_size = 512
-    loader = ImagesLoader(batch_size)
-    dataset = loader.get_dataset(training_paths, training=False)
+    lr          = 0.001
+    model       = Model()
+    backward    = backward_linear("adam", model)
 
-    lr = 0.001
-
-    model    = Model()
-    backward = backward_linear("sgd",model)
-
-   num_epoch = 50
+    num_epoch    = 50
 
     #Define loss function
-    criterion = CrossEntropyLoss()
-
-    batch_count_train = (60000 + batch_size) // batch_size
-    loss_chart = CreateGraph(batch_count_train, "MNIST Crossentropy loss")
+    criterion   = CrossEntropyLoss()
+    
+    #Class for showing loss on graph
+    batch_count_train   = (60000 + batch_size) // batch_size
+    loss_chart          = CreateGraph(batch_count_train, "MNIST Crossentropy loss")
 
     print("Training started...")
     count = 0
+    #Epoch loop
     for epoch in range(num_epoch):
+        #Batch loop
         for images, labels in (zip(*dataset)):
-
+            # Reshape from (batch,1,28,28) to (batch,784) for linear layer
             images = images.reshape(batch_size, 784)
-
+            # Push images to model
             y_pred = model.forward(images)
-
+            # Compute loss
             loss = criterion.forward(labels, y_pred)
-
+            # Graph adding
             loss_chart.num_for_avg += loss
-
+            #Run backward
             model = backward.run_backward(model, y_pred, labels, lr, images, epoch, criterion)
 
         loss_chart.count(epoch)
